5fca02d9a186c92768feec3533f2bde4772f04ae
luyandan <2103029766@qq.com>
2020-11-08 16:46:32 +0800

    初始化导航栏布局

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,88 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect
+} from "react-router-dom";
+import styled from "styled-components";
+
+const Wrapper=styled.div`
+  border:1px solid red;
+  min-height:100vh;
+  display:flex;
+  flex-direction:column;
+`;
+const Main=styled.div`
+ overflow:auto;
+  border:1px solid green;
+  flex-grow:1;
+`
+const Nav=styled.nav`
+  border:1px solid blue;
+  >ul{
+    display:flex;
+    >li{
+      width:33.333%;
+      text-align:center;
+      padding:16px;
+    }
+  }
+`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Wrapper>
+        <Main>
+        <Switch>
+          <Route path="/tags">
+            <Tags />
+          </Route>
+          <Route path="/money">
+            <Money />
+          </Route>
+          <Route path="/statics">
+            <Statics/>
+          </Route>
+          <Redirect exact from="/" to="/money"/> 
+          <Route path="*">
+            <NoMatch/>
+          </Route>
+        </Switch>
+        </Main>
+        <Nav>
+          <ul>
+            <li>
+              <Link to="/tags">标签页</Link>
+            </li>
+            <li>
+              <Link to="/money">记帐页</Link>
+            </li>
+            <li>
+              <Link to="/statics">统计页</Link>
+            </li>
+          </ul>
+        </Nav>
+      </Wrapper>
+    </Router>
   );
+}
+
+function Statics() {
+  return <h2>统计页面</h2>;
+}
+
+function Tags() {
+  return <h2>标签页面</h2>;
+}
+
+function Money() {
+  return <h2>记账页面</h2>;
+}
+function NoMatch(){
+  return <h1>你访问的页面不存在</h1>
 }
 
 export default App;
